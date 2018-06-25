ead9f3d247440b58d33365d9218002240c595935
jaggesher <jaggesher14@gmail.com>
2018-06-25 22:51:35 +0600

    new course view added

--- resources/views/All-Add/add-course.blade.php
+++ resources/views/All-Add/add-course.blade.php
@@ -14,7 +14,59 @@
 
 
 @section('ContentOfBody')
+<div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">New Course</div>
 
+                    <div class="panel-body">
+                        <form class="form-horizontal" method="POST" action="{{ route('course.submit') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="name" class="col-md-3 control-label">Course Name</label>
+
+                                <div class="col-md-8">
+                                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                    @if ($errors->has('name'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="description" class="col-md-3 control-label">Course Description</label>
+
+                                <div class="col-md-8">
+                                    <textarea id="description"  class="form-control" name="description" rows="8" cols="50"  required>{{ old('description') }}</textarea>
+
+                                    @if ($errors->has('description'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('description') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            
+
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-3">
+                                    <button type="submit" class="btn btn-primary">
+                                        Save
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 
